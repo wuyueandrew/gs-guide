7e929ccb3ff734872477d930e8b27dcc348e7361
wuyue <wuyue_andrew@outlook.com>
2023-03-30 14:15:31 +0800

    edit type of priority to char

--- test_io_archive.cc
+++ test_io_archive.cc
@@ -10,19 +10,17 @@ template <typename VID_T>
 struct OneidMsg {
   using vid_t = VID_T;
 
-  int type_val;
-  int priority;
+  char priority;
   std::vector<vid_t> connect_vertices;
   std::vector<double> connect_vertices_confi;
 
-  OneidMsg() : type_val(-1), priority(-1) {}
+  OneidMsg() : priority((char)0) {}
 
   ~OneidMsg() = default;
 
   // for message manager to serialize and diserialize
   friend grape::InArchive& operator<<(grape::InArchive& in_archive,
                                       const OneidMsg& u) {
-    in_archive << u.type_val;
     in_archive << u.priority;
     in_archive << u.connect_vertices;
     in_archive << u.connect_vertices_confi;
@@ -31,7 +29,6 @@ struct OneidMsg {
 
   friend grape::OutArchive& operator>>(grape::OutArchive& out_archive,
                                        OneidMsg& val) {
-    out_archive >> val.type_val;
     out_archive >> val.priority;
     out_archive >> val.connect_vertices;
     out_archive >> val.connect_vertices_confi;
@@ -58,13 +55,15 @@ int main() {
 
   // struct msg
   gs::OneidMsg<int64_t> msg;
-  msg.type_val = 1;
-  msg.priority = 2;
-  std::vector<int64_t> connect_vertices{1,2,3,4,5};
-  std::vector<double> connect_vertices_confi{1.0,1.0,1.0,1.0,1.0};
+  msg.priority = (char)1;
+  std::vector<int64_t> connect_vertices{100000000,100000001,100000002,100000003,100000004};
+  std::vector<double> connect_vertices_confi{1.0000,1.0000,1.0000,1.0000,1.0000};
   msg.connect_vertices = connect_vertices;
   msg.connect_vertices_confi = connect_vertices_confi;
   in << msg;
   std::cout<< "size: " << in.GetSize() << std::endl;
   std::cout<< "=====" << std::endl;
+
+  in.Clear();
+
 }
