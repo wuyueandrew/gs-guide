4d35b9b6a4e1563988056266bd97b4cff5ef239d
wuyueandrew <wuyue_andrew@outlook.com>
2023-03-30 10:57:58 +0800

    io archive

---
+++ test_io_archive.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+
+#include "grape/serialization/in_archive.h"
+#include "grape/serialization/out_archive.h"
+
+
+namespace gs {
+
+template <typename VID_T>
+struct OneidMsg {
+  using vid_t = VID_T;
+
+  int type_val;
+  int priority;
+  std::vector<vid_t> connect_vertices;
+  std::vector<double> connect_vertices_confi;
+
+  OneidMsg() : type_val(-1), priority(-1) {}
+
+  ~OneidMsg() = default;
+
+  // for message manager to serialize and diserialize
+  friend grape::InArchive& operator<<(grape::InArchive& in_archive,
+                                      const OneidMsg& u) {
+    in_archive << u.type_val;
+    in_archive << u.priority;
+    in_archive << u.connect_vertices;
+    in_archive << u.connect_vertices_confi;
+    return in_archive;
+  }
+
+  friend grape::OutArchive& operator>>(grape::OutArchive& out_archive,
+                                       OneidMsg& val) {
+    out_archive >> val.type_val;
+    out_archive >> val.priority;
+    out_archive >> val.connect_vertices;
+    out_archive >> val.connect_vertices_confi;
+    return out_archive;
+  }
+};
+
+
+}// namespace gs
+
+int main() {
+  grape::InArchive in;
+  size_t size = in.GetSize();
+  std::cout<< "size: " << size << std::endl;
+  gs::OneidMsg<int> msg;
+  msg.type_val = 1;
+  msg.priority = 2;
+  std::vector<int> connect_vertices{1,2,3,4,5};
+  std::vector<double> connect_vertices_confi{1.0,1.0,1.0,1.0,1.0};
+  msg.connect_vertices = connect_vertices;
+  msg.connect_vertices_confi = connect_vertices_confi;
+  in << msg;
+  std::cout<< "size: " << in.GetSize() << std::endl;
+  std::cout<< "=====" << std::endl;
+}
